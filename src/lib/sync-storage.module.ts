7af188ee9f833082b9a41d94019bb0274c102107
Mikhail Baranov <mikhail.baranov@orbitsoft.com>
2021-07-16 09:58:41 +0300

    Changed service providing

--- src/lib/sync-storage.module.ts
+++ src/lib/sync-storage.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 
 import { SyncStorageDirective } from './sync-storage.directive';
+import { SyncStorageService } from './sync-storage.service';
 
 @NgModule({
     declarations: [SyncStorageDirective],
-    imports: [
-    ],
-    exports: [SyncStorageDirective]
+    imports: [],
+    exports: [SyncStorageDirective],
+    providers: [SyncStorageService]
 })
 export class SyncStorageModule { }
--- src/lib/sync-storage.service.ts
+++ src/lib/sync-storage.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 
-@Injectable({
-    providedIn: 'root',
-})
+@Injectable()
 export class SyncStorageService {
     private storageSubjects: { [key: string]: BehaviorSubject<any> } = {};
 
